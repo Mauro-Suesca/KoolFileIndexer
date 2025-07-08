48e7c391f9c49acaab86dcd8e4ff72314c04b301
Mauro-Suesca <msuesca@unal.edu.co>
2025-07-07 22:31:25 -0500

    [ConectorBaseDeDatos.java] Creé una clase para hacer la conexión a postgresql desde java, pero eso modificó muchísimos archivos de gradle, este commit es una llamada de auxilio

---
+++ Proyecto/app/src/main/java/koolfileindexer/Archivo.java
@@ -0,0 +1,16 @@
+package koolfileindexer;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+public class Archivo {
+    String nombre;
+    long tamanoBytes;
+    LocalDateTime fechaCreacion;
+    String rutaCompleta;
+    String extension;
+    String categoria;
+    List<String> etiquetas;
+    Set<String> palabrasClave;
+}
