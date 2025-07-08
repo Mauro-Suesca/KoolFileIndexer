f29cf9fd3997a068022615b938d5c4fc728f9550
ThePixelCode <thepixelcode@proton.me>
2025-07-08 08:02:13 -0500

    koolfileindexer/ui: He creado TableComponent

--- Proyecto/app/src/main/java/koolfileindexer/ui/MainWindow.java
+++ Proyecto/app/src/main/java/koolfileindexer/ui/MainWindow.java
@@ -3,8 +3,11 @@ package koolfileindexer.ui;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import koolfileindexer.ui.components.SearchComponent;
+import koolfileindexer.ui.components.TableComponent;
 
 public class MainWindow extends Scene {
 
@@ -12,7 +15,13 @@ public class MainWindow extends Scene {
         // A grid panel with of 4 by 4
         VBox p = new VBox(10);
         p.setPadding(new Insets(10));
-        p.getChildren().add(new SearchComponent());
+        HBox searchBar = new SearchComponent();
+        p.getChildren().add(searchBar);
+        TableComponent table = new TableComponent();
+        p.getChildren().add(table);
+
+        VBox.setVgrow(searchBar, Priority.NEVER);
+        VBox.setVgrow(table, Priority.ALWAYS);
         return p;
     }
 
