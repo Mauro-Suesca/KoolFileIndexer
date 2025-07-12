1b0acf9de888f181f35ef20b256dc0842d0b5528
Mauro-Suesca <msuesca@unal.edu.co>
2025-07-11 19:54:05 -0500

    [Archivo.java] Añadí un comentario aclarativo al esqueleto de la clase Archivo

--- Proyecto/app/src/main/java/koolfileindexer/db/Archivo.java
+++ Proyecto/app/src/main/java/koolfileindexer/db/Archivo.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Archivo {
-
+    //Esqueleto básico de la clase Archivo que es necesario para las funciones del conector a la base de datos, será reemplazado por una clase completa eventualmente
     String nombre;
     long tamanoBytes;
     LocalDateTime fechaCreacion;
