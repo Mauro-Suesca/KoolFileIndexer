5e03a2d48c057607d3b2c9b4809a34f1a0d0b603
ThePixelCode <thepixelcode@proton.me>
2025-07-18 11:04:06 -0500

    KoolFileIndexer: He reordenado todo el projecto

--- Proyecto/app/src/main/java/koolfileindexer/App.java
+++ Proyecto/app/src/main/java/koolfileindexer/App.java
@@ -3,15 +3,18 @@ package koolfileindexer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import koolfileindexer.common.Constants;
 import koolfileindexer.controller.Controller;
 import koolfileindexer.ui.MainWindow;
 
 public class App extends Application {
 
     Controller controller = new Controller();
+    Integer unused = 0;
 
     public static void main(String[] args) {
         System.out.println("👋, 🌎❗");
+        System.out.println(Constants.APP_NAME);
         launch(args);
     }
 
---
+++ Proyecto/common/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module common {
+    exports koolfileindexer.common;
+}
