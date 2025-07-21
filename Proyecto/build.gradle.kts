cf36bc57fef478b2cc80152c948f5f85cc11e864
Juan Camargo <jucamargop@unal.edu.co>
2025-07-21 11:16:00 -0500

    [build.gradle.kts]: Se agregaron los linters

--- Proyecto/build.gradle.kts
+++ Proyecto/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.gradleup.shadow") version "8.3.8" apply false
     id("org.openjfx.javafxplugin") version "0.1.0" apply false
     id("com.diffplug.spotless") version "7.1.0"
+    id("checkstyle")
+    id("com.github.spotbugs") version "6.0.15" apply false
 }
 
 repositories {
@@ -25,8 +27,11 @@ allprojects {
     }
 }
 
+
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "checkstyle")
+    apply(plugin = "com.github.spotbugs")
 
     if (name != "common") {
         apply(plugin = "application")
@@ -41,6 +46,34 @@ subprojects {
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.9.2")
         "testImplementation"("org.mockito:mockito-core:5.3.1")
         "testImplementation"("org.mockito:mockito-junit-jupiter:5.3.1")
+    }
+    // Checkstyle
+    configure<CheckstyleExtension> {
+        toolVersion = "10.12.1"
+        configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+        isIgnoreFailures = false
+    }
+
+    tasks.withType<Checkstyle> {
+        reports {
+            html.required.set(true)
+            html.outputLocation.set(file("$buildDir/reports/checkstyle/${name}.html"))
+        }
+    }
+
+    // SpotBugs
+    configure<com.github.spotbugs.snom.SpotBugsExtension> {
+        toolVersion.set("4.8.5")
+        effort.set(com.github.spotbugs.snom.Effort.MAX)
+        reportLevel.set(com.github.spotbugs.snom.Confidence.LOW)
+        excludeFilter.set(file("$rootDir/config/spotbugs/excludeFilter.xml")) 
+    }
+
+    tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
+        reports.create("html") {
+            required.set(true)
+            outputLocation.set(file("$buildDir/reports/spotbugs/${name}.html"))
+        }
     }
 }
 
